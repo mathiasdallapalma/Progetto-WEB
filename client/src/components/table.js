811b3f615bb179e36ce1ccc23af05cd4c897eac6
Mathias Dalla Palma <mathiasdallapalma@gmail.com>
2024-06-09 21:51:33 +0200

    Responsive desing for Table

--- client/src/components/table.js
+++ client/src/components/table.js
@@ -12,31 +12,36 @@ import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 const apiProxy = 'http://localhost:4000';
 //const apiProxy = 'https://puppeteer-render-hb03.onrender.com';
 
-const Table = () => {
+var last_event = {
+    sortField: "None",
+    id: ""
+};
 
-    const columns = [
-        { id: 0, label: "Number", accessor: "ORD_NUM" },
-        { id: 1, label: "Amount", accessor: "ORD_AMOUNT" },
-        { id: 2, label: "Date", accessor: "ORD_DATE" },
-        { id: 3, label: "Customer", accessor: "CUST_CODE" },
-        { id: 4, label: "Agent", accessor: "AGENT_CODE" },
-        { id: 5, label: "Description", accessor: "ORD_DESCRIPTION" },
-    ];
+const columns = [
+    { id: 0, label: "Number", accessor: "ORD_NUM" },
+    { id: 1, label: "Amount", accessor: "ORD_AMOUNT" },
+    { id: 2, label: "Date", accessor: "ORD_DATE" },
+    { id: 3, label: "Customer", accessor: "CUST_CODE" },
+    { id: 4, label: "Agent", accessor: "AGENT_CODE" },
+    { id: 5, label: "Description", accessor: "ORD_DESCRIPTION" },
+];
+
+
+const Table = () => {
 
     const [tableData, setTableData] = useState([]);
     const [sortField, setSortField] = useState("");
-
     const [orderField, setOrderField] = useState([]);
 
+
     const resetOrderField = (except) => {
-        console.log("resetOrderField")
         for (let i = 0; i < columns.length; i++) {
             if (columns[i].id != except)
                 orderField[columns[i].id] = "default";
         }
     }
 
-    const fetchTours = async () => {
+    const fetchOrders = async () => {
         try {
             const response = (await axios.get(apiProxy + '/orders', { headers: { Authorization: Cookies.get('auth_token') } })).data;
             console.log(response);
@@ -53,10 +58,22 @@ const Table = () => {
         }
     };
 
+    const [width, setWidth] = useState(window.innerWidth);
 
     useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
 
-        fetchTours();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        fetchOrders();
         resetOrderField(-1);
 
     }, []);
@@ -74,20 +91,41 @@ const Table = () => {
         }
 
         if (sortField) {
-            const sorted = [...tableData].sort((a, b) => {
-                if (a[sortField] === null) return 1;
-                if (b[sortField] === null) return -1;
-                if (a[sortField] === null && b[sortField] === null) return 0;
-                return (
-                    a[sortField].toString().localeCompare(b[sortField].toString(), "en", {
-                        numeric: true,
-                    }) * (orderField[id] === "asc" ? 1 : -1)
-                );
-            });
-            setTableData(sorted);
+            sortTableData(sortField, id);
         }
         resetOrderField(id);
     };
+
+    const sortTableData = (sortField, id) => {
+
+        const sorted = [...tableData].sort((a, b) => {
+            if (a[sortField] === null) return 1;
+            if (b[sortField] === null) return -1;
+            if (a[sortField] === null && b[sortField] === null) return 0;
+            return (
+                a[sortField].toString().localeCompare(b[sortField].toString(), "en", {
+                    numeric: true,
+                }) * (orderField[id] === "asc" ? 1 : -1)
+            );
+        });
+
+        setTableData(sorted);
+
+    }
+
+
+    const handleSorting2 = (event) => {
+
+        const sortField = event.target.value;
+        const id = event.target.options[event.target.options.selectedIndex].id;
+
+        if (sortField != "None") {
+            sortTableData(sortField, id);
+        }
+        last_event.sortField = sortField;
+        last_event.id = id;
+
+    }
 
     const handleEdit = (data) => {
         console.log("edit")
@@ -104,78 +142,141 @@ const Table = () => {
     const agentCodeHandler = (code) => {
         console.log("agent")
         console.log(code)
-        
+
     };
 
 
     const custCodeHandler = (code) => {
         console.log("agent")
         console.log(code)
-        
+
     };
 
+    const handleSwitch = (event) => {
+        const isChecked = event.target.checked;
+
+        for (let i = 0; i < columns.length; i++) {
+            if (isChecked)
+                orderField[columns[i].id] = "desc";
+            else
+                orderField[columns[i].id] = "asc";
+        }
+
+        if(last_event.sortField!="None")
+            sortTableData(last_event.sortField, last_event.id);
+
+    }
+
+    if (width > 900) {
+        return (
+            <div className="tableDiv">
+                <table className="table">
+                    <caption>
+                        Developers currently enrolled in this course, column headers are sortable.
+                    </caption>
 
 
-    return (
-        <div className="tableDiv">
-            <table className="table">
-                <caption>
-                    Developers currently enrolled in this course, column headers are sortable.
-                </caption>
+                    <thead>
+                        <tr>
+                            {columns.map(({ id, label, accessor }) => {
 
+                                let cl = "tr"
 
-                <thead>
-                    <tr>
-                        {columns.map(({ id, label, accessor }) => {
+                                return (
+                                    <th
+                                        key={accessor}
+                                        onClick={() => handleSorting(accessor, id)}
+                                        className={cl}
+                                    >
+                                        {label}{orderField[id] === "asc" ? ` \u25B4` : ""}{orderField[id] === "desc" ? " \u25BE" : ""}
 
-                            let cl = "asd"
+                                    </th>
+                                );
+                            })}
+                            <th className="lastCol"></th>
+                        </tr>
+                    </thead>
 
+                    <tbody>
+                        {tableData.map((data) => {
                             return (
-                                <th
-                                    key={accessor}
-                                    onClick={() => handleSorting(accessor, id)}
-                                    className={cl}
-                                >
-                                    {label}{orderField[id] === "asc" ? ` \u25B4` : ""}{orderField[id] === "desc" ? " \u25BE" : ""}
+                                <tr key={data.id}>
+                                    {columns.map(({ accessor }) => {
+                                        const tData = data[accessor] ? data[accessor] : "——";
+                                        let clickHandler = null
+                                        if (accessor === "CUST_CODE") {
+                                            clickHandler = custCodeHandler
+                                        } else if (accessor === "AGENT_CODE") {
+                                            clickHandler = agentCodeHandler
+                                        }
 
-                                </th>
+                                        return <td style={{ cursor: clickHandler ? 'pointer' : 'default' }}
+                                            key={accessor + data.id}
+                                            onClick={clickHandler ? () => clickHandler(data[accessor]) : null}>
+                                            {tData}
+                                        </td>;
+                                    })}
+                                    <td style={{ width: '115 px' }}>
+                                        <KebabMenu data={data} handleEdit={handleEdit} handleDelete={handleDelete} />
+                                    </td>
+                                </tr>
                             );
                         })}
-                        <th></th>
-                    </tr>
-                </thead>
+                    </tbody>
 
-                <tbody>
-                    {tableData.map((data) => {
+                </table>
+            </div>
+
+        );
+    }
+    else {
+        return (<div className="tableDiv">
+            <div className="sortcontainer">
+
+                <label for="order-field">Sort by:</label>
+                <select id="order-field" name="order-field" onChange={handleSorting2}>
+                    <option id="-1" value="None">---</option>
+                    {columns.map(({ id, label, accessor }) => {
                         return (
-                            <tr key={data.id}>
-                                {columns.map(({ accessor }) => {
-                                    const tData = data[accessor] ? data[accessor] : "——";
-                                    let clickHandler=null
-                                    if(accessor==="CUST_CODE"){
-                                        clickHandler=custCodeHandler
-                                    }else if(accessor==="AGENT_CODE"){
-                                        clickHandler=agentCodeHandler
-                                    }
-
-                                    return <td style={{ cursor: clickHandler ? 'pointer' : 'default' }}
-                                                key={accessor + data.id} 
-                                                onClick={clickHandler ? () => clickHandler(data[accessor]) : null}>
-                                        {tData} 
-                                        </td>;
-                                })}
-                                <td>
-                                    <KebabMenu data={data} handleEdit={handleEdit} handleDelete={handleDelete} />
-                                </td>
-                            </tr>
+                            <option id={id} value={accessor}>{label}</option>
                         );
                     })}
-                </tbody>
+                </select>
 
-            </table>
-        </div>
+                <div class="sort-options">  Asc - Desc
+                    <label class="switch">
+                        <input type="checkbox" onChange={handleSwitch}></input>
+                        <span class="slider round"></span>
+                    </label>
+                </div>
 
-    );
+            </div>
+            {tableData.map((data) => {
+                return (
+                    <tr key={data.id}>
+                        {columns.map(({ accessor }) => {
+                            const tData = data[accessor] ? data[accessor] : "——";
+                            let clickHandler = null
+                            if (accessor === "CUST_CODE") {
+                                clickHandler = custCodeHandler
+                            } else if (accessor === "AGENT_CODE") {
+                                clickHandler = agentCodeHandler
+                            }
+
+                            return <td style={{ cursor: clickHandler ? 'pointer' : 'default' }}
+                                key={accessor + data.id}
+                                onClick={clickHandler ? () => clickHandler(data[accessor]) : null}>
+                                {tData}
+                            </td>;
+                        })}
+                        <td style={{ width: '115 px' }}>
+                            <KebabMenu data={data} handleEdit={handleEdit} handleDelete={handleDelete} />
+                        </td>
+                    </tr>
+                );
+            })}
+        </div>)
+    }
 };
 
 export default Table;
